e189cd478dcf0c57861c398b4226e148c46cd31c
河明宗 <akimune1003@gmail.com>
2019-04-01 22:32:14 +0900

    first commit

--- Memo/AppDelegate.swift
+++ Memo/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-Memo/Pods-Memo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Memo : NSObject
+@end
+@implementation PodsDummy_Pods_Memo
+@end
