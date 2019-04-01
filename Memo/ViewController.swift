d24c500100f1349227e34c58b0bb0b7f5d6b311a
河明宗 <akimune1003@gmail.com>
2019-04-01 22:15:41 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 河明宗 on 2019/04/01.
+//  Copyright © 2019 Akimune Kawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
