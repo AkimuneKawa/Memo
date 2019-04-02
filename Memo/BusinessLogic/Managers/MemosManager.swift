81497a3cf26b207db28c033b17cfcb0fe2795575
河明宗 <akimune1003@gmail.com>
2019-04-02 14:53:03 +0900

    Manager:MemosManagerを追加
    それに伴い、Utilities:Formatterを追加

---
+++ Memo/BusinessLogic/Managers/MemosManager.swift
@@ -0,0 +1,53 @@
+//
+//  MemosManager.swift
+//  Memo
+//
+//  Created by 河明宗 on 2019/04/02.
+//  Copyright © 2019 Akimune Kawa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class MemosManager {
+    let userDefaults = UserDefaults.standard
+    let userDefaultKey = "Memos"
+    
+    //MARK: - Methods
+    
+    private func loadMemos() -> [Memo] {
+        var retMemos: [Memo] = []
+        var unsortedMemos: Dictionary<String,[String]> = [:]
+        if let tmpMemos = userDefaults.dictionary(forKey: userDefaultKey) as? Dictionary<String,[String]> {
+            unsortedMemos = tmpMemos
+        }
+        
+        let sortedMemos = unsortedMemos.sorted{ $0.1[1] > $1.1[1] }
+        
+        for ( key , value) in sortedMemos {
+            var memo = Memo()
+            memo.id = key
+            memo.content = value[0]
+            memo.dateLastTouched = value[1]
+            
+            retMemos.append(memo)
+        }
+        
+        return retMemos
+    }
+    
+    private func saveMemo(memo: Memo) {
+        var savedMemos: Dictionary<String,[String]> = [:]
+        let dateLastTouched: String = DateFormatter.default.string(from: Date())
+        
+        if let tmpMemos = userDefaults.dictionary(forKey: userDefaultKey) as? Dictionary<String,[String]> {
+            savedMemos = tmpMemos
+        }
+        
+        savedMemos[memo.id] = [memo.content,dateLastTouched]
+        
+        userDefaults.set(savedMemos, forKey: userDefaultKey)
+    }
+}
---
+++ Memo/Utilities/Formatter.swift
@@ -0,0 +1,19 @@
+//
+//  Formatter.swift
+//  Memo
+//
+//  Created by 河明宗 on 2019/04/02.
+//  Copyright © 2019 Akimune Kawa. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let `default` : DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+}
